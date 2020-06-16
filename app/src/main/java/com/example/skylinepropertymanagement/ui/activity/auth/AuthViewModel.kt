8a1c186affaf3d1afd200f631bd4874fd38086ea
Seth <sethosrictorralba@gmail.com>
2020-06-16 16:47:08 -0500

    Login Activity Improved, LiveData User

--- app/src/main/java/com/example/skylinepropertymanagement/ui/activity/auth/AuthViewModel.kt
+++ app/src/main/java/com/example/skylinepropertymanagement/ui/activity/auth/AuthViewModel.kt
@@ -3,8 +3,6 @@ package com.example.skylinepropertymanagement.ui.activity.auth
 import android.view.View
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.skylinepropertymanagement.app.App
-import com.example.skylinepropertymanagement.app.log
 import com.example.skylinepropertymanagement.data.model.User
 import com.example.skylinepropertymanagement.data.repo.AuthRepo
 
@@ -15,26 +13,33 @@ class AuthViewModel:ViewModel() {
     var password:String? = null
     var type:Boolean? = true
     var repo = AuthRepo()
+//    var user = User()
     val loginResult by lazy {MutableLiveData<String>()}
     val typeSwitch by lazy {MutableLiveData<Boolean>()}
+    val user by lazy{
+        val x = MutableLiveData<User>()
+        x.value = User()
+        x
+    }
 
     fun loginButtonClicked(view: View) {
-        if(email.isNullOrEmpty()) {
+        if(user.value?.email.isNullOrEmpty()) {
             loginResult.value = "email"
             return
-        } else if(password.isNullOrEmpty()) {
+        } else if(user.value?.password.isNullOrEmpty()) {
             loginResult.value = "password"
             return
         }
-        repo.login(email!!, password!!, type!!)
-        App.instance.log(repo.login(email!!, password!!, type!!).toString())
+
+        repo.login(user)
+
         loginResult.value = "success"
 
     }
 
     fun switchToggled(view:View){
         typeSwitch.value = typeSwitch.value != true
-        type = typeSwitch.value
+        user.value?.type = typeSwitch.value!!
 
     }
 
