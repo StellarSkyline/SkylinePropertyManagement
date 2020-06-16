b5c68d12fc0e06411e00b85e9c35b62af814e752
Seth <sethosrictorralba@gmail.com>
2020-06-16 10:53:03 -0500

    Added Splash Screen

--- app/src/main/java/com/example/skylinepropertymanagement/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/skylinepropertymanagement/ui/activity/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.skylinepropertymanagement.ui.activity
 
+import android.content.Intent
+import android.content.Intent.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.example.skylinepropertymanagement.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        init()
+    }
+
+    private fun init() {
+        var intent = Intent(this, LoginActivity::class.java)
+
+        var handler = Handler()
+
+        handler.postDelayed({
+            intent.flags = FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
+        }, 3000)
+
     }
 }
